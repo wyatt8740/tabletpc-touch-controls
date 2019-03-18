8a72df8777b9c4f9acc46278158cae440ab2fe70
wyatt8740 <wyatt8740@users.noreply.github.com>
2019-03-18 06:39:41 -0400

    lots of tweaks

--- applet.py
+++ applet.py
@@ -2,18 +2,19 @@
 import gi
 import subprocess # to execute appropriate scripts/programs
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 
-btnsize="48" # change this to change the size of button icons.
+btnsize="32" # change this to change the size of button icons.
 
 APP_NAME="TabletPC_Applet_Menu"
 
 class TabletApplet(Gtk.Window):
     def __init__(self):
-        Gtk.Window.__init__(self, title="Tablet Functions")
+        Gtk.Window.__init__(self, title="Tablet Controls")
         self.set_wmclass(APP_NAME,APP_NAME)
 
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+#        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        vbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         self.add(vbox)
 
 #        button=Gtk.Button(label="Window Switcher")
@@ -23,17 +24,17 @@ class TabletApplet(Gtk.Window):
         button.connect("clicked", self.winList)
         vbox.pack_start(button, True, True, 0)
         
-        button=Gtk.Button(label="Keyboard")
+        button=Gtk.Button(label="\nKeyboard\n")
         button.connect("clicked", self.toggleKeybd)
         vbox.pack_start(button, True, True, 0)
 
 #       touch digitizer on/off
-        button=Gtk.Button(label="Toggle multitouch")
+        button=Gtk.Button(label="\nTouch\n")
         button.connect("clicked", self.toggleTouch)
         vbox.pack_start(button, True, True, 0)
 
 #       unclutter on/off
-        button=Gtk.Button(label="Toggle unclutter")
+        button=Gtk.Button(label="Clutter")
         button.connect("clicked", self.toggleUnclutter)
         vbox.pack_start(button, True, True, 0)
 
@@ -52,11 +53,11 @@ class TabletApplet(Gtk.Window):
         vbox.pack_start(button, True, True, 0)
 
 #       calibrate
-        button=Gtk.Button(label="Calib")
+        button=Gtk.Button(label="\n Calibrate \n")
         button.connect("clicked", self.calib)
         vbox.pack_start(button, True, True, 0)
 
-        button=Gtk.Button(label="Exit")
+        button=Gtk.Button(label=" ✖ ")
         button.connect("clicked", Gtk.main_quit)
         vbox.pack_start(button, True, True, 0)
 #        self.add(self.btnKeybd)
@@ -86,5 +87,10 @@ class TabletApplet(Gtk.Window):
 
 win=TabletApplet()
 win.connect("destroy", Gtk.main_quit)
+scr=Gdk.Display.get_default().get_default_screen()
+# very wm specific hardcoded geometry stuff here
+win.move(0, scr.get_height() - (win.get_size().height) / 2 + 32)
+#print(scr.get_height())
+#print(win.get_scale_factor())
 win.show_all()
 Gtk.main()
